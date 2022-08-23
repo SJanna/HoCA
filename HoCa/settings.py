976d1f2ccb6a7ea972cb409e7d73411d2653949c
SJanna <jannas@uninorte.edu.co>
2022-08-23 16:12:56 -0500

    general settings

--- HoCa/settings.py
+++ HoCa/settings.py
@@ -144,3 +144,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+"import django_heroku" 
+"django_heroku.settings(locals())"
