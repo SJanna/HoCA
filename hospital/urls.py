82f81b5edc3f39e83ffe344a0e672386dbc89459
SJanna <jannas@uninorte.edu.co>
2022-08-29 03:12:00 -0500

    urls config

--- hospital/urls.py
+++ hospital/urls.py
@@ -4,7 +4,7 @@ from .views import PerfilPaciente,PerfilPersonalSalud, Inicio
 urlpatterns = [
     path('perfil_paciente/', PerfilPaciente.as_view(), name="perfil_paciente"),
     path('perfil_personal_salud/', PerfilPersonalSalud.as_view(), name="perfil_personal_salud"),
-    path('api/', include('hospital.routers')),
-    path('', Inicio, name="inicio"),
+    path('', include('hospital.routers')),
+    path('inicio/', Inicio, name="inicio"),
     path('', include('rest_framework.urls', namespace='rest_framework')),
 ]
