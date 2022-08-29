afa14dd65222b26201806649bb8633860d9a8d8a
SJanna <jannas@uninorte.edu.co>
2022-08-29 02:28:19 -0500

    Debug=False

--- HoCa/settings/base.py
+++ HoCa/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rr4y9@p#r2t)jp%i@r41!h@vg3v&wtfsq1c)(x=*m%pz7d7f5^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
